8b9ea4f22f706fc98e3940681102bc8ba8424902
Alex Beattie <artrisanbranding@me.com>
2019-12-30 03:55:22 -0800

    Initial Commit

---
+++ hgh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hgh
+//
+//  Created by Alex Beattie on 12/30/19.
+//  Copyright © 2019 Alex Beattie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
